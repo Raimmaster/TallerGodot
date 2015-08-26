904831c8e4aaa57b2683e1bf09ea3c53b504fd44
Raim <rai.interiano@gmail.com>
2015-08-25 18:54:44 -0600

    More animation examples

---
+++ level1.gd
@@ -0,0 +1,22 @@
+
+extends Node2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+var animacion = ""
+var mover_righ = Input.is_action_pressed("move_right")
+
+func _ready():
+	# Initialization here
+	set_process(true)
+	
+	
+	
+func _process(delta):
+	if(mover_righ):
+		animacion = "run"
+	else:
+		animacion = "stand"
+	get_node("personaje_s/AnimationPlayer").play(animacion)
